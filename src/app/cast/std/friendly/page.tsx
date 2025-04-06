a47de3a8987f5a3727ba511b6d708782aee3317b
ta1pre <taichiumeki@gmail.com>
2025-04-06 20:07:52 +0900

    Add friendly cast landing page with components and LINE login button

---
+++ src/app/cast/std/friendly/page.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import React from 'react';
+import { HeroSection } from './components/HeroSection';
+import { RewardSystem } from './components/RewardSystem';
+import { ThreeReasons } from './components/ThreeReasons';
+import { EarningMethods } from './components/EarningMethods';
+import { HowItWorks } from './components/HowItWorks';
+import { Registration } from './components/Registration';
+import { ClientFooter } from '@/components/common';
+import { LineLoginButton } from '@/features/LineAuth';
+
+export default function FriendlyPage() {
+  return (
+    <div className="min-h-screen flex flex-col bg-white relative">
+      <main className="flex-grow flex flex-col items-center">
+        <HeroSection />
+        <RewardSystem />
+        <ThreeReasons />
+        <EarningMethods />
+        <HowItWorks />
+        <Registration />
+      </main>
+      
+      <ClientFooter className="mb-16" />
+      
+      {/* LINEログインボタン */}
+      <div className="fixed bottom-0 left-0 right-0 z-50 w-full">
+        <div className="w-full bg-white pt-3 pb-3 border-t border-gray-200">
+          <p className="text-center text-red-600 font-bold mb-2">
+            ＼ もれなく1万円分のボーナスポイント進呈中！ ／
+          </p>
+          <LineLoginButton />
+        </div>
+      </div>
+    </div>
+  );
+}
